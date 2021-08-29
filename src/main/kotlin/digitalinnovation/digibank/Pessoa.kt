bd8e3e3ca49efb927ed02f4566ade1e26608f70c
joaoatlas <joao.alves@atlastechnol.com>
2021-08-29 13:22:42 -0300

    feat: cria clase Conta.kt

--- src/main/kotlin/digitalinnovation/digibank/Pessoa.kt
+++ src/main/kotlin/digitalinnovation/digibank/Pessoa.kt
@@ -4,10 +4,13 @@ class Pessoa {
     var nome: String = "Joao"
     var cpf: String = "123.456.789-89"
     private set
+
+    constructor()
+
+    fun pessoaInfo() = "Nome: $nome, CPF: $cpf "
 }
 
 fun main(){
     val joao = Pessoa()
-    println(joao.nome)
-    println(joao.cpf)
+    println(joao.pessoaInfo())
 }
